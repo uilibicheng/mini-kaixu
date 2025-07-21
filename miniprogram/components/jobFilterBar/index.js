aa7620505001bf03ed3ebd8c0944fde0333e7372
ian01.li <ian01.li@vipshop.com>
2025-07-21 20:52:45 +0800

    feat: 岗位搜索页

---
+++ miniprogram/components/jobFilterBar/index.js
@@ -0,0 +1,31 @@
+// components/jobFilterBar/index.ts
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    background: {
+      type: String,
+      value: ''
+    },
+    color: {
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
