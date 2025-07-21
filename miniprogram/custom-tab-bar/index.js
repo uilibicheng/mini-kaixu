1202e839c2591b7833b9641c8bf64fb4fafe327f
ian01.li <ian01.li@vipshop.com>
2025-07-21 18:30:51 +0800

    feat: 完成职位详情页开发

--- miniprogram/custom-tab-bar/index.js
+++ miniprogram/custom-tab-bar/index.js
@@ -6,26 +6,26 @@ Component({
     list: [
       {
         pagePath: "/pages/index/index",
-        iconPath: "/assets/images/tabbar/job-active.svg",
+        iconPath: "/assets/images/tabbar/job.svg",
         selectedIconPath: "/assets/images/tabbar/job-active.svg",
         text: "职位",
       },
       {
         pagePath: "/pages/notice/notice",
         iconPath: "/assets/images/tabbar/notice.svg",
-        selectedIconPath: "/assets/images/tabbar/notice.svg",
+        selectedIconPath: "/assets/images/tabbar/notice-active.svg",
         text: "公告",
       },
       {
         pagePath: "/pages/message/message",
         iconPath: "/assets/images/tabbar/message.svg",
-        selectedIconPath: "/assets/images/tabbar/message.svg",
+        selectedIconPath: "/assets/images/tabbar/message-active.svg",
         text: "消息",
       },
       {
         pagePath: "/pages/mine/mine",
         iconPath: "/assets/images/tabbar/mine.svg",
-        selectedIconPath: "/assets/images/tabbar/mine.svg",
+        selectedIconPath: "/assets/images/tabbar/mine-active.svg",
         text: "我的",
       },
     ],
---
+++ miniprogram/pages/jobDetail/components/jobBanner/index.js
@@ -0,0 +1,31 @@
+// pages/jobDetail/components/jobBanner/index.ts
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    currentPic: 0,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onSlideChange(e) {
+      const { current } = e.detail
+      this.setData({
+        currentPic: current,
+      })
+    },
+  }
+})
